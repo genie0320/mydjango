790d43b6f7efdde4f8f2d58e80dc2d829072a125
Genie <genie0320@gmail.com>
2024-03-21 10:46:07 +0900

    add Posts list / link

--- post/urls.py
+++ post/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list),
+    path('', views.post_list, name='post'),
+    path('<slug:slug>', views.post_page, name='page'), #<>사이에 빈칸이 있으면 에러난다.
 ]
--- post/views.py
+++ post/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .models import Post
+from django.http import HttpResponse
 
 # Create your views here.
 def post_list(request):
-    return render(request,'post/post_list.html')
+    posts = Post.objects.all().order_by('-date')
+    return render(request,'post/post_list.html', {'posts':posts})
+def post_page(request, slug):
+    # posts = Post.objects.all().order_by('-date')
+    return HttpResponse(slug)
+
